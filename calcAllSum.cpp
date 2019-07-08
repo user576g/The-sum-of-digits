892b79af3fc614e096b8ad564ab6aaeea0314f8d
user576g <52647767+user576g@users.noreply.github.com>
2019-07-08 16:26:37 +0300

    Add files via upload

---
+++ calcAllSum.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+const int n = 15;
+
+typedef  unsigned long long unll;
+
+inline void setArValue(unsigned char* ar, unsigned long long val) {
+	unsigned long long d = val;
+	int i = 0;
+	for (; d != 0; ++i) {
+		ar[n - 1 - i] = d % 10;
+		d /= 10;
+	}
+	for (; i < n; ++i)
+		ar[n - 1 - i] = 0;
+}
+
+int main() {
+	unll A, B;
+	cout << "\n\nInput first number of range: ";
+	cin >> A;
+	cout << "Input last number of range: ";
+	cin >> B;
+	if (A > B){
+		cout << "Last number can`t be less than first\n\n";
+		return 0;
+	}
+	unsigned char arCount[n];
+	unsigned char arB[n];
+	setArValue(arCount, A);
+	setArValue(arB, B);
+
+	int countB = n;
+	while (arB[n - countB] == 0 && countB > 0)
+		--countB;
+	unsigned long long i, sum = 0;
+	for (i = A; i <= B; ++i) {
+		for (int j = n - 1; j >= (n - countB); --j) {
+			sum += arCount[j];
+		}
+		for (int j = n - 1; j >= (n - countB); --j) {
+			if (arCount[j] < 9) {
+				++arCount[j];
+				break;
+			}
+			else
+				arCount[j] = 0;
+		}
+	}
+	cout << "The sum of digits of all numbers in range: " << sum << "\n\n";
+}
